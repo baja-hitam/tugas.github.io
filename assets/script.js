b8e3c115dae3dc80b54e34000c0cb5c383eb4566
baja-hitam <104021660+baja-hitam@users.noreply.github.com>
2022-08-29 17:57:59 +0700

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,18 @@ let dropdown = document.querySelector('.dropdown');
 let menu = document.querySelectorAll('.menu');
 let caret = document.querySelector('.caret');
 let [tab] = menu;
+const bodyHtml = document.getElementsByTagName('main')[0];
+
+const menuDown = () => {
+  tab.classList.toggle('menu-open');
+  caret.classList.toggle('caret-close');
+}
 
   dropdown.addEventListener('click', () => {
-    tab.classList.toggle('menu-open');
-    caret.classList.toggle('caret-close');
-  })
+    menuDown();
+  });
+
+  bodyHtml.addEventListener('click', () => {
+    listMenu = tab.classList.contains('menu-open');
+    (!listMenu) ? "" : menuDown();
+  });
